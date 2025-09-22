b16a34111801998b155188310429db59156e3b5c
ZyadRizk <zyadh3545@gmail.com>
2025-09-22 14:16:21 +0300

    Initial commit

---
+++ ECommerceApp/ECommerce.Domain/Entities/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities
+{
+    internal class Orders
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public required string ShippingAddress { get; set; }
+        public required string PaymentMethod { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+    }
+}
